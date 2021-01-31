c19d0c0c9d5be2dacd3ad96f832a6a70be925b11
Pabeloszh <Pabeloszh@gmail.com>
2021-01-31 17:56:12 +0100

    img path error repaired

--- data.js
+++ data.js
@@ -1,74 +1,74 @@
 //Player------------------------------------------------------
 const playerImg = new Image();
-playerImg.src = "../data/PNG/playerShip1_blue.png";
+playerImg.src = "./img/PNG/playerShip1_blue.png";
 
 //Enemies------------------------------------------------------
 const enemy1 = new Image();
-enemy1.src = "../data/PNG/Enemies/enemyBlack1.png";
+enemy1.src = "./img/PNG/Enemies/enemyBlack1.png";
 const enemy2 = new Image();
-enemy2.src = "../data/PNG/Enemies/enemyBlack2.png";
+enemy2.src = "./img/PNG/Enemies/enemyBlack2.png";
 const enemy3 = new Image();
-enemy3.src = "../data/PNG/Enemies/enemyBlack3.png";
+enemy3.src = "./img/PNG/Enemies/enemyBlack3.png";
 const enemy4 = new Image();
-enemy4.src = "../data/PNG/Enemies/enemyBlack4.png";
+enemy4.src = "./img/PNG/Enemies/enemyBlack4.png";
 const enemy5 = new Image();
-enemy5.src = "../data/PNG/Enemies/enemyBlack5.png";
+enemy5.src = "./img/PNG/Enemies/enemyBlack5.png";
 
 //Lasers------------------------------------------------------
 const playerLaserImg = new Image();
-playerLaserImg.src = "../data/PNG/Lasers/laserBlue01.png";
+playerLaserImg.src = "./img/PNG/Lasers/laserBlue01.png";
 const enemyLaserImg = new Image();
-enemyLaserImg.src = "../data/PNG/Lasers/laserPurple01.png";
+enemyLaserImg.src = "./img/PNG/Lasers/laserPurple01.png";
 
 //PowerUps------------------------------------------------------
 const pow1 = new Image();
-pow1.src = "../data/PNG/Power-ups/powerupBlue_star.png";
+pow1.src = "./img/PNG/Power-ups/powerupBlue_star.png";
 const pow2 = new Image();
-pow2.src = "../data/PNG/Power-ups/powerupBlue_shield.png";
+pow2.src = "./img/PNG/Power-ups/powerupBlue_shield.png";
 const pow3 = new Image();
-pow3.src = "../data/PNG/Power-ups/powerupBlue_bolt.png";
+pow3.src = "./img/PNG/Power-ups/powerupBlue_bolt.png";
 const powerImgArr = [pow1, pow2, pow3];
 
 //Shield------------------------------------------------------
 const shield = new Image();
-shield.src = "../data/PNG/Effects/shield3.png";
+shield.src = "../img/PNG/Effects/shield3.png";
 
 //Meteors------------------------------------------------------
 const meteor1 = new Image();
-meteor1.src = "../data/PNG/Meteors/meteorBrown_big1.png";
+meteor1.src = "./img/PNG/Meteors/meteorBrown_big1.png";
 const meteor2 = new Image();
-meteor2.src = "../data/PNG/Meteors/meteorBrown_big2.png";
+meteor2.src = "./img/PNG/Meteors/meteorBrown_big2.png";
 const meteor3 = new Image();
-meteor3.src = "../data/PNG/Meteors/meteorBrown_big3.png";
+meteor3.src = "./img/PNG/Meteors/meteorBrown_big3.png";
 const meteorsImgArr = [meteor1, meteor2, meteor3];
 
 //Icons------------------------------------------------------
 const shieldIcon = new Image();
-shieldIcon.src = "../data/PNG/Power-ups/shield_silver.png";
+shieldIcon.src = "./img/PNG/Power-ups/shield_silver.png";
 const healthIcon = new Image();
-healthIcon.src = "../data/PNG/UI/playerLife1_blue.png";
+healthIcon.src = "./img/PNG/UI/playerLife1_blue.png";
 
 //Numeral------------------------------------------------------
 const numeral0 = new Image();
-numeral0.src = "../data/PNG/UI/numeral0.png";
+numeral0.src = "./img/PNG/UI/numeral0.png";
 const numeral1 = new Image();
-numeral1.src = "../data/PNG/UI/numeral1.png";
+numeral1.src = "./img/PNG/UI/numeral1.png";
 const numeral2 = new Image();
-numeral2.src = "../data/PNG/UI/numeral2.png";
+numeral2.src = "./img/PNG/UI/numeral2.png";
 const numeral3 = new Image();
-numeral3.src = "../data/PNG/UI/numeral3.png";
+numeral3.src = "./img/PNG/UI/numeral3.png";
 const numeral4 = new Image();
-numeral4.src = "../data/PNG/UI/numeral4.png";
+numeral4.src = "./img/PNG/UI/numeral4.png";
 const numeral5 = new Image();
-numeral5.src = "../data/PNG/UI/numeral5.png";
+numeral5.src = "./img/PNG/UI/numeral5.png";
 const numeral6 = new Image();
-numeral6.src = "../data/PNG/UI/numeral6.png";
+numeral6.src = "./img/PNG/UI/numeral6.png";
 const numeral7 = new Image();
-numeral7.src = "../data/PNG/UI/numeral7.png";
+numeral7.src = "./img/PNG/UI/numeral7.png";
 const numeral8 = new Image();
-numeral8.src = "../data/PNG/UI/numeral8.png";
+numeral8.src = "./img/PNG/UI/numeral8.png";
 const numeral9 = new Image();
-numeral9.src = "../data/PNG/UI/numeral9.png";
+numeral9.src = "./img/PNG/UI/numeral9.png";
 
 //Levels------------------------------------------------------
 const lvlOne = [
